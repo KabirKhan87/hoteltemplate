36a14af4216d71080ff1924a83ecfd094a20d04a
kabir khan87 <kabirkhan80k@gmail.com>
2026-01-30 14:15:31 +0530

    fd

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
      return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+     return render_template('about.html')
+
+@app.route('/room.html')
+def room():
+     return render_template('room.html')
  
 if __name__ == '__main__':
     app.run()
